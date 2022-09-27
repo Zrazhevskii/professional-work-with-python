163c66ce9a3fda77896f8539bc309fc84cb03ec9
Zrazhevskii Denis <vbn-134@yandex.ru>
2022-09-27 12:45:54 +0300

    add_functions_sieve_of_Eratosthenes_and_get_employees

--- application/db/people.py
+++ application/db/people.py
@@ -1,2 +1,14 @@
+import numpy as np
+
 def get_employees():
-    pass
+    print('В нашей организации работает 150 человек')
+
+def sieve_of_Eratosthenes():
+    N = int(input('Введите число для вычесления простых чисел '))
+    a = np.array(range(3,N,2))
+    for j in range(0, int(round(np.sqrt(N),0))):
+        a[(a!=a[j]) & (a%a[j] == 0)] = 0
+        a = a[a!=0]
+    a = [2]+list(a)
+    print(a)
+
