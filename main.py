dd59cad5712544dba2f7e910f84abca25436b3e9
Zrazhevskii Denis <vbn-134@yandex.ru>
2022-09-28 14:11:51 +0300

    add_functions

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
+from datetime import date
+import application.salary
+import application.db.people
 
+
+def main():
+    current_date = date.today()
+    print(f'Сегодня {current_date}')
+
+
+if __name__ == '__main__':
+    main()
+    application.salary.calculate_salary()
+    application.db.people.get_employees()
+    # application.db.people.sieve_of_Eratosthenes()
