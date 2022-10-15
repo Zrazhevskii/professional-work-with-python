65c5bbe926b549eb7483a52696d524f4da8f6743
Zrazhevskii Denis <vbn-134@yandex.ru>
2022-10-15 20:34:57 +0300

    add_function_parametrized_decor

--- decorators/decorator.py
+++ decorators/decorator.py
@@ -1,17 +1,19 @@
 from datetime import datetime
 
 
-def decorator(some_function):
-    def new_function(some_list):
-        log_date = datetime.now().strftime("%d %B %Y  time %H:%M:%S")
-        function = some_function(some_list)
-        result = f'Вызвана функция {some_function.__name__}, время вызова функции {log_date}.\n' \
-                 f'Вводные данные: {some_list}\n' \
-                 f'Результат выполнения функции {some_function.__name__} - {function}\n'\
-                 f'                                                                    \n'
+def parametrized_decor(params):
+    def decorator(some_function):
+        def new_function(*args, **kwars):
+            log_date = datetime.now().strftime("%d %B %Y  time %H:%M:%S")
+            function = some_function(*args, **kwars)
+            result = f'Вызвана функция {some_function.__name__}, время вызова функции {log_date}.\n' \
+                     f'Вводные данные: {args}, {kwars}\n' \
+                     f'Результат выполнения функции {some_function.__name__} - {function}\n'
 
-        with open('date_log.txt', 'a', encoding='utf-8') as f:
-            f.write(result)
-        return some_function
+            with open('date_log.txt', 'w', encoding='utf-8') as f:
+                f.write(result)
+            return function
 
-    return new_function
+        return new_function
+
+    return decorator
